ff5f62d3dd79f7cbcb26f8a49c6296cf63976d76
whayven <waynefosterjr@hotmail.com>
2022-12-30 15:33:07 -0500

    removed restricted characters from page

--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -37,12 +37,12 @@ export default function Uses() {
             <div className="space-y-20">
                 <ToolsSection title="Workstation">
                     <Tool title="16” MacBook Pro">
-                        Transitioning from Windows, the Apple ecosystem is amazing for productivity and I’ve never
+                        Transitioning from Windows, the Apple ecosystem is amazing for productivity and I&apos;ve never
                         heard the fans turn on a  single time, even under the incredibly heavy loads I put it
                         through.
                     </Tool>
                     <Tool title="Samsung 27” Curved Monitor">
-                        A classic curved monitor for extra viewing pleasure.  It's at a reasonable price point
+                        A classic curved monitor for extra viewing pleasure.  It&apos;s at a reasonable price point
                         but makes some compromises in resolution and refresh rate to compensate. Still, I find it be a
                         great monitor for the price.
                     </Tool>
@@ -54,9 +54,9 @@ export default function Uses() {
                 </ToolsSection>
                 <ToolsSection title="Development tools">
                     <Tool title="Visual Studio Code">
-                        This isn't a full-blown IDE but VSCode is iconic and basically a staple in the toolkit of any developer.
+                        This isn&apos;t a full-blown IDE but VSCode is iconic and basically a staple in the toolkit of any developer.
                         With a variety of extensions, extensive customization options, shortcuts, and more, I find this to be one of
-                        my most versatile programs that's capable of dealing with many problems outside of writing code.
+                        my most versatile programs that&apos;s capable of dealing with many problems outside of writing code.
                     </Tool>
                     <Tool title="iTerm2">
                         I’m honestly not even sure what features I get with this that
@@ -74,7 +74,7 @@ export default function Uses() {
                     <Tool title="Notion">
                         This has been a great tool for Documentation and staying organized.  There are so many components
                         to take advantage of that also complement my notes really well.  I would recommend this to anyone, or
-                        Evernote as an alternative (but it doesn't have as many components to use).
+                        Evernote as an alternative (but it doesn&apos;t have as many components to use).
                     </Tool>
                 </ToolsSection>
             </div>
