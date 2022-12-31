7d2285bfb5e65bdd68311360e82e40b1385541f4
Wayne <waynefosterjr@hotmail.com>
2022-12-31 10:30:02 -0500

    added image support for SSG

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ import rehypePrism from '@mapbox/rehype-prism'
 const nextConfig = {
   pageExtensions: ['jsx', 'mdx'],
   reactStrictMode: true,
+  images: {
+    loader:'imgix',
+    path: '',
+    domains: ['localhost', 'cw-dev-stack-cloudwaydev31d63348-s6nokm8dvki6.s3.amazonaws.com'],
+    formats: ['image/avif', 'image/webp'],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840]
+  },
   experimental: {
     scrollRestoration: true,
   },
